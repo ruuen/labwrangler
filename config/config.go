dc6f0885d95dea81c6b2936230ecd78215386dc7
Max Ruuen <max@madebyruuen.com>
2025-11-08 19:14:44 +1100

    add config setting wrapper
    will load from cmd and env right now, adding config file in future

---
+++ config/config.go
@@ -0,0 +1,85 @@
+package config
+
+import (
+	// "errors"
+	"flag"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	Settings map[string]string
+}
+
+type setting struct {
+	name string
+	cmdKey string
+	cmdDescription string
+	envKey string
+	configKey string
+	required bool
+	value string
+}
+
+func New() (s *AppConfig) {
+	return &AppConfig{
+		Settings: make(map[string]string),
+	}
+}
+
+func (c *AppConfig) Init() error {
+	settings := []*setting{
+		{
+			name: "AppId",
+			envKey: "DISCORD_APP_ID",
+			configKey: "appId",
+			required: true,
+		},
+		{
+			name: "AppToken",
+			envKey: "DISCORD_APP_TOKEN",
+			configKey: "appToken",
+			required: true,
+		},
+		{
+			name: "GuildId",
+			cmdKey: "guildId",
+			cmdDescription: "Application Token of Discord bot",
+			envKey: "DISCORD_GUILD_ID",
+			configKey: "guildId",
+			required: true,
+		},
+	}
+
+	// cmdline params
+	for _, v := range settings {
+		if v.cmdKey != "" {
+			flag.StringVar(&v.value, v.cmdKey, "", v.cmdDescription)
+		}
+	}
+	flag.Parse()
+
+	// env vars
+	godotenv.Load()
+	for _, v := range settings {
+		if v.value == "" && v.envKey != "" {
+			env := os.Getenv(v.envKey)
+			v.value = env
+		}
+	}
+
+	// config file (TBC)
+
+	// app defaults (TBC)
+
+	for _, v := range settings {
+		// if v.required == true && v.value == "" {
+		// 	return errors.New("Value was not provided for " + v.name)
+		// }
+
+		c.Settings[v.name] = v.value
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,45 +1,50 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 	"github.com/ruuen/labwrangler/commands"
+	"github.com/ruuen/labwrangler/config"
 )
 
 func main() {
 	// Config settings; priority: cmdline->env vars->config file
 	// TODO: add a wrapper struct for app's config settings and get this out of main
-	var guildId string
-	var appId string
-	var appToken string
-	flag.StringVar(&guildId, "guildId", "", "Guild ID to monitor")
-	flag.Parse()
+	// var guildId string
+	// var appId string
+	// var appToken string
+	// flag.StringVar(&guildId, "guildId", "", "Guild ID to monitor")
+	// flag.Parse()
+	//
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Unable to load environment variables: %v", err)
+	// }
+	// appId = os.Getenv("DISCORD_APP_ID")
+	// appToken = os.Getenv("DISCORD_APP_TOKEN")
+	// if guildId == "" {
+	// 	guildId = os.Getenv("DISCORD_GUILD_ID")
+	// }
 
-	err := godotenv.Load()
+	config := config.New()
+	err := config.Init()
 	if err != nil {
-		log.Fatalf("Unable to load environment variables: %v", err)
-	}
-	appId = os.Getenv("DISCORD_APP_ID")
-	appToken = os.Getenv("DISCORD_APP_TOKEN")
-	if guildId == "" {
-		guildId = os.Getenv("DISCORD_GUILD_ID")
+		log.Fatalln(err)
 	}
 
 	// TODO: this needs later improvement with the config wrapper to dynamically check and return missing settings. im the only dunce who would forget right now so it's fine :)
-	switch {
-	case appId == "":
-	case appToken == "":
-	case guildId == "":
-		log.Fatalf("You haven't provided a configuration setting. I'll leave it up to you to guess which one. Good luck!")
-	}
+	// switch {
+	// case appId == "":
+	// case appToken == "":
+	// case guildId == "":
+	// 	log.Fatalf("You haven't provided a configuration setting. I'll leave it up to you to guess which one. Good luck!")
+	// }
 
-	s, err := discordgo.New("Bot " + appToken)
+	s, err := discordgo.New("Bot " + config.Settings["AppToken"])
 	if err != nil {
 		log.Fatalf("Unable to open Discord session: %v", err)
 	}
@@ -49,7 +54,7 @@ func main() {
 		&commands.ChannelClearCommand,
 	}
 
-	createdCommands, err := s.ApplicationCommandBulkOverwrite(appId, guildId, targetCommands)
+	createdCommands, err := s.ApplicationCommandBulkOverwrite(config.Settings["AppId"], config.Settings["GuildId"], targetCommands)
 	if err != nil {
 		log.Fatalf("Failed to create command: %v", err)
 	}
